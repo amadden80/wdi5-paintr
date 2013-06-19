aa1ccea5c65fb4fcf3691b26a13e32f8a865b878
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-06-19 16:41:41 -0400

    Add color functionality... added

--- app/assets/javascripts/paintr.js
+++ app/assets/javascripts/paintr.js
@@ -1,6 +1,11 @@
 //Called when the Add Color button is pressed, creates a new swatch of color in our pallette, with the inputted colo
 function add_color(){
-
+  var color_choice = $('#color-input').val();
+  var new_swatch = $('<div>');
+  new_swatch.addClass('swatch');
+  new_swatch.css('background-color', color_choice);
+  $('#pallette').append(new_swatch);
+  $('#color-input').val('');
 }
 
 // Called when a swatch of color is clicked, changes the selected color to that color
@@ -15,9 +20,11 @@ function draw(){
 
 //Called when document loads, fills the canvas with .paintdot div elements
 function populate_canvas(){
-  var dot = $('<div>');
-  dot.addClass('paint-dot');
-  $('#canvas').append(dot);
+  for (var i = 0; i<1200; i++){
+    var dot = $('<div>');
+    dot.addClass('paint-dot');
+    $('#canvas').append(dot);
+  }
 }
 
 // Called when the clear button is clicked, remove all of the swatches in the pallete and clears the selected color
@@ -32,11 +39,14 @@ function clear_pallette(){
 //Alternate (pretty) way to run code after DOM load
 $(function(){
   populate_canvas();
-  // Add Color event
+  $('#add-color').click(add_color);
   // Clear event
   // Canvas hover event
   // Swatch click event
 });
+
+
+
 
 
 
