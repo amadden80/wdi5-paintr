a3b1410db9debe7d2becd343e1d8ad8719effac9
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-06-19 16:19:10 -0400

    Adding skeleton functions to js and populating one dot

--- app/assets/javascripts/paintr.js
+++ app/assets/javascripts/paintr.js
@@ -1,12 +1,48 @@
-// Define your functions here
+//Called when the Add Color button is pressed, creates a new swatch of color in our pallette, with the inputted colo
+function add_color(){
+
+}
+
+// Called when a swatch of color is clicked, changes the selected color to that color
+function select_color(){
+
+}
+
+//Called when the user hovers over the canvas, changes the color of hovered paint dots to the selected color
+function draw(){
+
+}
+
+//Called when document loads, fills the canvas with .paintdot div elements
+function populate_canvas(){
+  var dot = $('<div>');
+  dot.addClass('paint-dot');
+  $('#canvas').append(dot);
+}
+
+// Called when the clear button is clicked, remove all of the swatches in the pallete and clears the selected color
+function clear_pallette(){
+
+}
 
 
-//Bind your events here
-$(document).ready(function(){
-  console.log('The DOM is loaded!');
-});
+
+
 
 //Alternate (pretty) way to run code after DOM load
-// $(function(){
-//   console.log('The DOM is loaded (v2)!');
-// });
+$(function(){
+  populate_canvas();
+  // Add Color event
+  // Clear event
+  // Canvas hover event
+  // Swatch click event
+});
+
+
+
+
+
+
+
+
+
